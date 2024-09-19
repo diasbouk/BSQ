48a42bd78cc86f796b45f2fafe0f71cb0a32eb51
diasbouk <boukdsaid@gmail.com>
2024-09-19 23:46:51 +0100

    header update

--- bsq.h
+++ bsq.h
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 
 
-
+/* Macros */
 #define BUFFER_SIZE 1024
 
-
+/* Strings funcs */
 void	_puts(const char *str);
-char *_strdup_till(const char *str, int (*func)(char));
-char	*read_file(int file);
 unsigned int _strlen(const char *str);
+char **_split(const char *str, const char *charset);
+
+/* Files funcs */
+char	*read_file(int file);
+
+/* Memory funcs */
 void _bzero(void *addr, unsigned int size);
 void	*ft_memcpy(void *dest, void *src, unsigned int size);
 void	*ft_realloc(void *old,unsigned int old_size, unsigned int new_size);
-char **_split(const char *str, const char *charset);
