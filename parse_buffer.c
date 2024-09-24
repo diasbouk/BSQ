c1c3e10d1949c2a36b02dc2a7ae2a095a8f78797
diasbouk <boukdsaid@gmail.com>
2024-09-24 13:34:36 +0100

    parsing file content in buffer

--- parse_buffer.c
+++ parse_buffer.c
@@ -29,22 +29,35 @@ static int	is_valid_line(const char *line)
 	return (1);
 }
 
-map_st	*parse_line(const char *line)
+void	free_d(char **list)
+{
+	int	i;
+
+	i = 0;
+	while (list[i++])
+		free(list[i]);
+	free(list);
+}
+
+map_st	*parse_buffer(const char *buffer)
 {
 	map_st *map;
 	int	i;
 	int	len;
+	char	*line;
 
-	if (!is_valid_line(line))
-		return (NULL);
 	map = (map_st *)malloc(sizeof(map_st));
 	if (!map)
+		return (NULL);
+	map->lines = _split(buffer, "\n");
+	line = map->lines[0];
+	if (!is_valid_line(line))
 		return (NULL);
 	map->height = 0;
 	len = _strlen(line);
 	i = 0;
 	while (i < len - 3)
-		map->height = (map->height * 10 ) + (line[i++] - 48);
+		map->height = (map->height * 10) + (line[i++] - 48);
 	map->empty = line[len - 3];
 	map->obstacle = line[len - 2];
 	map->full = line[len - 1];
