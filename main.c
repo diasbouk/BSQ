41f4729fa39b7e7b4df4d77536d14f6016afc0c3
diasbouk <boukdsaid@gmail.com>
2024-09-27 10:35:39 +0100

    Added option for multiple files

--- main.c
+++ main.c
@@ -1,31 +1,60 @@
 #include "bsq.h"
 
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
 	char	*buffer;
 	int i;
+	int j;
 	point pt;
 	map_st *map;
 
 	(void)ac;
-	if (av[1])
-		buffer = read_file(open(av[1], O_RDWR));
-	else
-		buffer = read_file(STDIN_FILENO);
-	if (!buffer)
-		return (0);
-	map = parse_buffer(buffer);
-	if (!map)
+	j = 1;
+	if (!av[1])
 	{
-		_puts("map ERROR\n");
+		buffer = read_file(STDIN_FILENO);
+		map = parse_buffer(buffer);
+		if (!map)
+		{
+			_puts("map ERROR\n");
+			exit(0);
+		}
+		pt = solve_map(map);
+		draw_solution(map, pt);
+		i = 0;
+		while (map->lines[i])
+		{
+			_puts(map->lines[i++]);
+			_puts("\n");
+		}
 		exit(0);
 	}
-	pt = solve_map(map);
-	draw_solution(map, pt);
-	i = 0;
-	while (map->lines[i])
+	else
 	{
-		_puts(map->lines[i++]);
-		_puts("\n");
+		while (av[j])
+		{
+			buffer = read_file(open(av[j], O_RDWR));
+			if (!buffer)
+			{
+				_puts("map ERROR\n");
+				exit(0);
+			}
+			map = parse_buffer(buffer);
+			if (!map)
+			{
+				_puts("map ERROR\n");
+				exit(0);
+			}
+			pt = solve_map(map);
+			draw_solution(map, pt);
+			i = 0;
+			while (map->lines[i])
+			{
+				_puts(map->lines[i++]);
+				_puts("\n");
+			}
+			j++;
+		}
 	}
 
 	return (0);
